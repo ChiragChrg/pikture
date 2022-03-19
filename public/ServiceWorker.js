dcb55ce79794146311b6dc86d8ee602eec7d7f23
ChiragChrg <chiruchirag2001@gmail.com>
2022-03-20 00:49:26 +0530

    Removed Offline.html & other cache files

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -1,21 +1,5 @@
-const CacheName = "V-2.4";
-const CacheList = [
-  "/",
-  "assets/icons/48.png",
-  "assets/icons/72.png",
-  "assets/icons/96.png",
-  "assets/icons/128.png",
-  "assets/icons/144.png",
-  "assets/icons/192.png",
-  "assets/icons/384.png",
-  "assets/icons/512.png",
-  "assets/icons/icon.js",
-  "assets/icons/favicon.svg",
-  "index.html",
-  "offline.html",
-  "manifest.json",
-  "ServiceWorker.js",
-];
+const CacheName = "V-2.5";
+const CacheList = ["/", "index.html", "manifest.json", "ServiceWorker.js"];
 
 //Installing Service Worker
 self.addEventListener("install", (event) => {
